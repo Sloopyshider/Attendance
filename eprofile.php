1056da20052bc0a2e01d8a60a4dc22fdc11e6e5e
unknown <ubasjake20@gmail.com>
2020-01-10 14:16:22 +0800

    EmployeeProfile

---
+++ eprofile.php
@@ -0,0 +1,40 @@
+<head>
+
+    <title> Employee Profile </title>
+    <link href="cssfiles/main.css" rel="stylesheet" type="text/css">
+    <link href="../css/theme.css" rel="stylesheet" type="text/css">
+    <link href="../css/theme.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+    <div class="head1">
+        <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+        <img class="img1" src="sections/companylogogreen.jpg" alt="comp">
+        <?php
+        if(isset($_SESSION['user'])){
+            echo '
+            
+ <label class="employeename">
+        <br>    Hello: &nbsp; '.$user['name'].'
+        <br>
+        Intern:    &nbsp      '.$user['pos'].'
+        <br>
+        Number:      &nbsp   '.$user['contact'].'
+        <br>
+
+        <button class="btn" href="logout.php"> Logout</button>
+        <a href="sections/logout.php">Log out</a>
+       			
+									
+									
+					
+    </label>
+         
+                         ';
+        }
+
+
+        ?>
+
+    </div>
+</body>
+
