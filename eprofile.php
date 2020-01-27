236377d3b456e43fb68115aac0e31ca80e9c874d
unknown <ubasjake20@gmail.com>
2020-01-27 18:46:14 +0800

    E Profile almost done, still working on hashing password

--- eprofile.php
+++ eprofile.php
@@ -1,6 +1,9 @@
 <?php
-include 'sections/navbar2.php';
 include "sections/session.php";
+include 'sections/navbar2.php';
+
+
+
 
 if(isset($_POST['update_profile'])){
 
@@ -13,11 +16,13 @@ if(isset($_POST['update_profile'])){
     $address = $_POST ['address'];
     $city = $_POST ['city'];
     $pass = $_POST ['pass'];
-    $confirmpass = $_POST['confirmpass'];
-    $new_pass = $_POST['new_pass'];
     $email_add = $_POST ['email_add'];
     $posit = $_POST ['posit'];
     $numb = $_POST ['numb'];
+    $con_pass = $_POST ['con_pass'];
+
+$pass = password_hash('$pass', PASSWORD_DEFAULT);
+
 
 
 
@@ -26,8 +31,6 @@ if(isset($_POST['update_profile'])){
                     `username`=:username,
                     `email`=:email_add,
                     `password`=:pass,
-                    `new_pass`=:new_pass,
-                    `confirm_pass`=:confirmpass,
                     `name`=:fname,
                     `midname`=:mid_name,
                     `lastname`=:last_name,
@@ -43,8 +46,6 @@ if(isset($_POST['update_profile'])){
         ':username' => $username,
         ':email_add' => $email_add,
         ':pass' => $pass,
-        ':new_pass'=> $new_pass,
-        ':confirmpass' => $confirmpass,
         ':fname' => $fname,
         ':mid_name' => $mid_name,
         ':last_name' => $last_name,
@@ -59,6 +60,8 @@ if(isset($_POST['update_profile'])){
 
     $stmt =  $conn->prepare($stmt);
     $pdoExc =  $stmt->execute($parameters);
+
+
 
 
 
@@ -99,17 +102,7 @@ $email_add = $user ['email'];
 $userPositionId = $user['pos'];
 $numb = $user ['contact'];
 $pass = $user ['password'];
-$new_pass = $user ['new_pass'];
-$confirmpass = $user ['confirmpass'];
-
-
-
-
-
-
-
-
-
+$con_pass = $user ['password'];
 
 
 
@@ -133,88 +126,91 @@ $confirmpass = $user ['confirmpass'];
 
 
 
-    echo "
+echo "
         
 <hr>
 <label class='userdetails'> User Details </label>
 <form action='eprofile.php' method='post'> 
 
 <label
-    <label class='personal'> Personal Info </label>
+    <label class='personal'></label>
     <table class='table2'>
          <th>
                 <input type='hidden' placeholder='id' name='id' id='id' value='$userId'>          
-                <input type='text' placeholder='Edit your Username' id='username' readonly name='username'  style='width: 100%' value='$userName'/>
+                Username <input type='text' placeholder='Edit your Username' id='username' readonly name='username'  style='width: 100%' value='$userName'/>
             <br>
             <br>
-                <input type='text' placeholder='Put your First Name' id='fname'  readonly name='fname'  style='width: 100%'  value='$firstname'/>
+                First name<input type='text' placeholder='Put your First Name' id='fname'  readonly name='fname'  style='width: 100%'  value='$firstname'/>
             <br>
             <br>
-                <input type='text' placeholder='Put your Middle Name' id='mid_name' readonly name='mid_name'   style='width: 100%' value='$mid_name'/>
+                Middle Name<input type='text' placeholder='Put your Middle Name' id='mid_name' readonly name='mid_name'   style='width: 100%' value='$mid_name'/>
             <br>
             <br>
-                <input type='text' placeholder='Put your Last Name' id='last_name' readonly name='last_name' style='width: 100%' value='$last_name'/>
+                Last Name<input type='text' placeholder='Put your Last Name' id='last_name' readonly name='last_name' style='width: 100%' value='$last_name'/>
             <br>
             <br>  
-                <input type='date' placeholder='Select Birthdate' id='bday' readonly name='bday'  style='width: 100%' value='$bday'/>
+                Birthday<input type='date' placeholder='Select Birthdate' id='bday' readonly name='bday'  style='width: 100%' value='$bday'/>
             <br>
             <br>
-                <input type='text' placeholder='Street/Block/Subdv No.' id='address' readonly name='address'   style='width: 100%' value='$address'/>
+                Address<input type='text' placeholder='Street/Block/Subdv No.' id='address' readonly name='address'   style='width: 100%' value='$address'/>
             <br>
             <br>
-                <input type='text' placeholder='City' readonly name='city' id='city'   style='width: 100%' value='$city'/>
+                City<input type='text' placeholder='City' readonly name='city' id='city'   style='width: 100%' value='$city'/>
        </th>
 
    
     </table>
-    <label class='account'> Account Info </label>
+    
     <table class='table3'>
-        <th>
-                <input type='password' placeholder='Input your Old Password' readonly name='pass' id='opass' style='width: 100%' value='$pass'/>
+        <th> 
+        
+                Password<input type='password' placeholder='Update your Password' readonly name='pass' onkeyup='check();' id='pass' style='width: 100%' value='$pass'/>
             <br> <br>
-            <input type='password' placeholder='Input your new Password' readonly name='new_pass' id='new_pass' style='width: 100%' value='$new_pass'/>
-             <br> <br>
-                <input type='password' placeholder='Type again your new password' readonly name='confirmpass' id='confirmpass' value='$confirmpass' style='width: 100%'/>
+            Confirm Password<input type='password' placeholder='Re-type your New Password' readonly name='con_pass' onkeyup='check();' id='con_pass' style='width: 100%' value='$pass'/>
+            <br> <br>
+            
         </th>
         
-     <label class='compinfo'> Company Info </label>
+    
     <table class='table4'>
         <th>
-                <input type='email' placeholder='Put your Email'  readonly name='email_add' id='email_add' style='width: 100%' value='$email_add'/>
+                Email<input type='email' placeholder='Put your Email'  readonly name='email_add' id='email_add' style='width: 100%' value='$email_add'/>
             <br>
             <br>";
 
 
 
-    $positionDropdown = "<select id='posit' name='posit' style='width: 100%' required disabled>";
+$positionDropdown = "Position<select id='posit' name='posit' style='width: 100%' required disabled>";
 
-    foreach ($positions as $position) {
-        $positionId = $position['id'];
-        $posName = $position['posname'];
-        $selected = ($positionId == $userPositionId) ? 'selected': '';
+foreach ($positions as $position) {
+    $positionId = $position['id'];
+    $posName = $position['posname'];
+    $selected = ($positionId == $userPositionId) ? 'selected': '';
 
-        $positionDropdown .= "<option value='$positionId' $selected>$posName</option>";
-    }
-    $positionDropdown .= "</select>";
-    echo $positionDropdown;
+    $positionDropdown .= "<option value='$positionId' $selected>$posName</option>";
+}
+$positionDropdown .= "</select>";
+echo $positionDropdown;
 
 
-    echo "
+echo "
             <br>
             <br>
-                <input type='number' placeholder='Please put your number' readonly name='numb' id='numb' style='width: 100%' value='$numb'/>
+                Mobile Number<input type='tel' placeholder='Please put your number' readonly name='numb' id='numb' style='width: 100%' value='$numb'/>
             <br>
         </th> 
     
     
-        <button class='sbmt' name='update_profile' onclick='return activate()' id='editButton' value='1'> EDIT </button>
+        <button class='edit' name='update_profile' onclick='return activateFields()' id='editButton' value='1'> EDIT </button>
+        <button class='create' name='create_profile' id='createButton' value='1'> Create new user </button>
         
             </table>
 </form>";
 
 
 
-    echo "
+
+echo "
             <script type='text/javascript'>
            
                 
@@ -231,53 +227,40 @@ $confirmpass = $user ['confirmpass'];
                     pass: '$pass',
                     email_add: '$email_add',
                     numb: '$numb',
-                  
-                  
-                    
+                    con_pass: '$con_pass',
+                                       
                 };
-            
-        
                 
-                
-                
-    
-                
-                function activate() {
+      
+                function activateFields() {
            
                     let textFields = [
-                        'username', 'fname', 'mid_name', 'last_name', 'bday', 'address', 'city','confirmpass','new_pass', 'pass', 'email_add', 'numb'
-                    ];   
+                        'username', 'fname', 'mid_name', 'last_name', 'bday', 'address', 'city', 'pass', 'email_add', 'numb', 'con_pass'];  
                     if(editable) { 
                         if(!confirm('Are you sure you want to update this data?')) {
                             textFields.forEach(textField => {
                                    document.getElementById(textField).value = originalValues[textField];
+                                     
                             });
                             return false;
                         }
-                        return true;
-                        
-//                            let x = confirm('Are you sure you want to update this data?');
-//                            
-//                                if (x === true){    
-//                                    editable = true;
-//                                }else {
-//                                    window.location.reload(true);
-//                                    return false;
-//                                }                    
+                        return true;  
                         } else
                             textFields.forEach(textField => {
                                 document.getElementById(textField).readOnly = false;
-                                });
+                                   
+                        });
+                           
                             document.getElementById('posit').disabled = false;
                             document.getElementById('editButton').innerHTML = 'Update';
                             editable = true;
-                            return false;
-                        }
+                            return false;         
+                        }   
                         
-                       
+                        
                     </script>
         ";
-    ?>
+?>
 
 
 
